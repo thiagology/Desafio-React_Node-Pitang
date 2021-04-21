a0bf638096275e26a250a28835ccd86b324806ae
Thiago Ferreira <thiagology@users.noreply.github.com>
2021-04-21 12:06:23 -0300

    Add handleCancel

--- src/components/Agendamento/AgendamentoCard.jsx
+++ src/components/Agendamento/AgendamentoCard.jsx
@@ -1,36 +1,11 @@
 /* eslint-disable arrow-body-style */
-import swal from '@sweetalert/with-react';
+
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
-import Modal from '../Modal';
 
-const CardAgendamento = ({ name, date, hour }) => {
-  const handleCancel = () => {
-    swal({
-      title: 'Cancelar agendamento',
-      text: 'Tem certeza que deseja cancelar?',
-      icon: 'warning',
-      buttons: ['Não', 'Sim'],
-      dangerMode: true,
-    })
-      .then((willDelete) => {
-        if (willDelete) {
-          swal({
-            title: 'Agendamento cancelado!',
-            icon: 'success',
-          });
-        } else {
-          swal({
-            title: 'Agendamento mantido!',
-            icon: 'success',
-          });
-        }
-      });
-  };
-
-  const handleConclusion = () => {
-    <Modal />;
-  };
+const CardAgendamento = ({
+  name, date, hour, handleConclusion, handleCancel,
+}) => {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
--- src/components/Agendamento/AgendamentoList.jsx
+++ src/components/Agendamento/AgendamentoList.jsx
@@ -1,13 +1,53 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable arrow-body-style */
+import swal from '@sweetalert/with-react';
 import React, { useContext } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { AgendamentoContext } from '../../pages/Agendamentos/AgendamentoContext';
+import axios from '../../utils/api';
 import Card from './AgendamentoCard';
 
 const AgendamentoList = () => {
   const [agendamentos, setAgendamentos] = useContext(AgendamentoContext);
+
+  const handleCancel = ({ _id }) => {
+    swal({
+      title: 'Cancelar agendamento',
+      text: 'Tem certeza que deseja cancelar?',
+      icon: 'warning',
+      buttons: ['Não', 'Sim'],
+      dangerMode: true,
+    })
+      .then(async (willDelete) => {
+        if (willDelete) {
+          try {
+            const newAgendamentos = agendamentos.filter((agendamento) => agendamento._id !== _id);
+            await axios.delete(`/agendamentos/${_id}`);
+            setAgendamentos(newAgendamentos);
+            swal({
+              title: 'Agendamento cancelado!',
+              icon: 'success',
+            });
+          } catch (error) {
+            swal({
+              title: 'Erro',
+              text: `${error.message}`,
+              icon: 'warning',
+            });
+          }
+        } else {
+          swal({
+            title: 'Agendamento mantido!',
+            icon: 'success',
+          });
+        }
+      });
+  };
+
+  const handleConclusion = () => {
+    console.log('Conclusion');
+  };
 
   return (
     <Container>
@@ -20,6 +60,8 @@ const AgendamentoList = () => {
                   name={agendamento.name}
                   date={agendamento.date}
                   hour={agendamento.hour}
+                  handleCancel={() => handleCancel(agendamento)}
+                  handleConclusion={() => handleConclusion}
                 />
               </Col>
 
